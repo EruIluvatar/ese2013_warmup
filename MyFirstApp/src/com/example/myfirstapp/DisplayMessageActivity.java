4c884998109047b59b786c64c4ddad4b791296d0
Stefan <stefan.borer@students.unibe.ch>
2013-09-29 00:02:48 +0200

    My first App

--- MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapp;
 
 import android.annotation.SuppressLint;
+//import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
